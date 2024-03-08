850661738c10aa14463c92b4a866c892b003c2f8
Viren Dhanwani <viren@acidtango.com>
2024-03-08 15:54:46 +0000

    feat: read and study

--- src/morning_routine.py
+++ src/morning_routine.py
@@ -8,5 +8,7 @@ class MorningRoutine:
     def what_should_i_do_now(self) -> str:
         if time(6) <= self.now < time(7):
             return "Do exercise"
+        if time(7) <= self.now < time(8):
+            return "Read and study"
 
         return ""
--- tests/test_morning_routine.py
+++ tests/test_morning_routine.py
@@ -25,3 +25,27 @@ def test_read_and_study_at_start_of_range():
     result = morning_routine.what_should_i_do_now()
 
     assert result == "Read and study"
+
+
+def test_read_and_study_at_end_of_range():
+    morning_routine = MorningRoutine(time(7, 59, 59))
+
+    result = morning_routine.what_should_i_do_now()
+
+    assert result == "Read and study"
+
+
+def test_have_breakfast_at_start_of_range():
+    morning_routine = MorningRoutine(time(8))
+
+    result = morning_routine.what_should_i_do_now()
+
+    assert result == "Have breakfast"
+
+
+def test_have_breakfast_at_end_of_range():
+    morning_routine = MorningRoutine(time(8, 59, 59))
+
+    result = morning_routine.what_should_i_do_now()
+
+    assert result == "Have breakfast"
