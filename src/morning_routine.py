27405b7d5dfaee638ab3bb916bd6fd3eeae79846
Manuel Escalante <maescalantehe@gmail.com>
2024-03-08 11:48:07 -0400

    second commit

--- src/morning_routine.py
+++ src/morning_routine.py
@@ -1,3 +1,11 @@
+from datetime import time
+
+
 class MorningRoutine:
+    def __init__(self, now: time) -> None:
+        self.now = now
+
     def what_should_i_do_now(self) -> str:
+        if (time(6) < self.now < time(7)):
+            return "Do exercise"
         return ""
--- tests/test_morning_routine.py
+++ tests/test_morning_routine.py
@@ -1,9 +1,21 @@
 from src.morning_routine import MorningRoutine
+from datetime import time
 
 
 def test_do_exercise():
-    morning_routine = MorningRoutine()
+    morning_routine = MorningRoutine(time(6,45))
 
     result = morning_routine.what_should_i_do_now()
 
     assert result == "Do exercise"
+
+def test_do_exercise_6_o_clock():
+    morning_routine = MorningRoutine(time(6))
+
+    result = morning_routine.what_should_i_do_now()
+
+    assert result == "Do exercise"
+
+
+def test_read_and_study():
+    pass
