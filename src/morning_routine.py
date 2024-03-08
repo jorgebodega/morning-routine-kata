a27671469c371e2efe3377e3fc81de90cf48494e
Manuel Escalante <maescalantehe@gmail.com>
2024-03-08 12:19:31 -0400

    wip

--- src/morning_routine.py
+++ src/morning_routine.py
@@ -7,7 +7,8 @@ class MorningRoutine:
 
     def what_should_i_do_now(self) -> str:
         activities = {
-            (time(6), time(7)): "Do exercise",
+            (time(6), time(6, 45)): "Do exercise",
+            (time(6,45), time(7)): "Take a shower",
             (time(7), time(7, 30)): "Read",
             (time(7, 30), time(8)): "Study",
             (time(8), time(9)): "Have breakfast",
